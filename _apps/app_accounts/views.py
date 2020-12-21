9498d205b5222678fe02fccdcbcca46d8b0cc30d
aamdo <a0555170767@gmail.com>
2020-12-21 14:14:47 +0300

    v9-templates

--- _apps/app_accounts/views.py
+++ _apps/app_accounts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     context = {}
     return render(request, 'accounts_home.html',context)
+
+def about(request):
+    context = {}
+    return HttpResponse('<h2>' + 'نظام معلومات الدوائر بمنطقة القصيم' + '</h2>')
+
 
