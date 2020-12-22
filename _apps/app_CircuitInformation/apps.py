30a07a7da4b52bfea81229c2fe80a6dd1e05eaee
a0555170767 <a0555170767@gmail.com>
2020-12-22 22:21:46 +0300

    dj 21

---
+++ _apps/app_CircuitInformation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppCircuitinformationConfig(AppConfig):
+    name = 'app_CircuitInformation'
--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+#from zipfile import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,4 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'project/static'),
+    ]
+
